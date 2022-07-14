ebb63530f4c65de3d2e90609601f64f1c2a234e3
randerthe <randerlealt@gmail.com>
2022-07-15 00:29:54 +0100

    FirtsCommit

---
+++ FindFileInFolderAndSubfolders/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace FindFileInFolderAndSubfolders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FindInMultipleFolders();
+        }
+
+
+        public static void FindInMultipleFolders()
+        {
+            string searchName = "XXXXXXX";
+            string[] files = Directory.GetFiles(@"XXXXXXXX", "*.*", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                FileInfo info = new FileInfo(file);
+                if (Path.GetFileNameWithoutExtension(info.Name).ToLower() == searchName.ToLower())
+                {
+                    Console.WriteLine(info.FullName);
+                }
+            }
+        }
+    }
+}
